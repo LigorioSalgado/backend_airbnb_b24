85ac690906dcce6f9be8e642b0dea8512b3a763e
LigorioSalgado <salgadolg5@hotmail.com>
2018-10-19 20:26:52 -0500

    calculate price

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const  express =  require("express");
 const router = express.Router();
 const isAuthenticated =  require("../middlewares/isAuthenticated");
+const {calculatePriceValidation} = require("../middlewares/validations")
 const {signUp,logIn} =  require("../controllers/users");
-const {createHouse,getAllHouses,getOneHouse} =  require("../controllers/houses")
+const {createHouse,getAllHouses,getOneHouse} =  require("../controllers/houses");
+const {calculatePrice,createBooking} = require('../controllers/bookings');
 
 //router.use('/users/',require('./user'));
 
@@ -13,6 +15,10 @@ router.post('/users/login',logIn)
 router.post('/houses',isAuthenticated,createHouse)
 router.get('/houses',getAllHouses)
 router.get('/houses/:id',getOneHouse)
+
+router.post('/bookings/calculate',calculatePriceValidation,calculatePrice);
+
+
 
 module.exports = router;
 
--- server/server.js
+++ server/server.js
@@ -8,13 +8,14 @@ const app =  express();
 
 const port  = process.env.PORT || 9000 
 
+const {errors} = require("celebrate");
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/api/v1',routes);
-
+app.use(errors());
 
 app.get('/', (req,res) => {
     res.send("Everthing Works! :)");
